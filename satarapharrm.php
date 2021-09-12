a6514dec23b19d2838f9b4fceca8412594c8c720
abhikadam27 <78306477+abhikadam27@users.noreply.github.com>
2021-09-12 16:59:16 +0530

    Add files via upload

---
+++ satarapharrm.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title> satara pharmacy</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+
+</head>
+</body>
+<div class="container-fluid px-0"> 
+ 	<?php
+		require_once"header.php";
+	?>
+<!--close navbar-->
+
+<div class="row" style="height:40px"> 
+<div class="col-1" style="background-color:#0080FF;" > <i class="bi bi-megaphone-fill" style="color:#FFFFFF"> News</i>
+</div> 
+ <div class="col-10"> <marquee bgcolor="#FFFFFF" direction="left"> <img src="image/new-icon-gif-2.jpg" style="height:30px; width:30px" /> D.pharmacy Admission Enquiry 2020-21 <img src="image/new-icon-gif-2.jpg" style="height:30px; width:30px" />  ||  <img src="image/new-icon-gif-2.jpg" style="height:30px; width:30px" /> B.pharmacy Admission Enquiry 2020-21 <img src="image/new-icon-gif-2.jpg" style="height:30px; width:30px" /> || <img src="image/new-icon-gif-2.jpg" style="height:30px; width:30px" /> M.pharmacy Admission Enquiry 2020-21 <img src="image/new-icon-gif-2.jpg" style="height:30px; width:30px" />|| <img src="image/new-icon-gif-2.jpg" style="height:30px; width:30px" /> second year D.pharmacy toppera 2020-21|| <img src="image/new-icon-gif-2.jpg" style="height:30px; width:30px" /> first year B.pharmacy Toppers<img src="image/new-icon-gif-2.jpg" style="height:30px; width:30px" /></marquee></div>
+<div class="col-1"> &nbsp;</div></div>
+
+<div class="container-fluid px-0">
+<div class="row">
+
+	<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/WhatsApp Image 2021-09-01 at 7.46.43 PM(2).jpeg" class="d-block w-100" alt="..." height="550px">
+    </div>
+    <div class="carousel-item">
+      <img src="image/WhatsApp Image 2021-09-01 at 7.46.43 PM(1).jpeg" class="d-block w-100" alt="..." height="550px">
+    </div>
+    <div class="carousel-item">
+      <img src="image/WhatsApp Image 2021-09-01 at 7.46.43 PM.jpeg" class="d-block w-100" alt="..." height="550px">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"> </span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div></div></div>
+<!--slider close-->
+<div class="row"><div class="col-12">
+<div class="alert alert-success" role="alert" style=" background-color:#00FFCC" >
+  <h3 align="center" style="color:#FFFFFF"> Welcome to satara college of pharmacy,satara<a href="#" class="alert-link"></a>
+</div></div></div>
+<!--close alert-->
+
+<div class="container-fluid">
+<div class="row">
+<div class="col-1"> &nbsp;</div>
+<div class="col-5"><h3 align="left">ABOUT US <hr style="color: #009999"/> </h3> <img src="image/satarapharmacy_home.png" class="rounded float-start" alt="..." width="280px" height="150" align="left" style="margin:20px"> <h6> Satara College of Pharmacy, Satara was started in the year 1999 by our beloved Chairman, Prof. M. S. Jagtap. It is located in a sprawling 10 acres campus at the foot of the pictures que hill. The campus surrounding environment is lush green, healthy and studious. It is about 5 kms from NH-4 (Mumbai - Bangalore).
+
+Today the college is recognised as one of the centres of academic excellence in the country. Over the years, there has been dynamic progress at SCOP in all academic and research activities, and a parallel improvement in facilities and infrastructure, to keep it on par with the best college in the country.The ideas on which the college is built evolve and change with national aspirations and trends world wide. Consciously developed, the campus has retained and increased its green cover, rich in natural flora and fauna. The peaceful atmosphere of the campus belies the full range of activities going on, that complements academic life.</h6></div>
+
+</div></div><br />
+  <!--close about-->
+  <div class="container-fluid">
+  <div class="row" style="background-color:#EEEEEE">
+  <div class="col-1">&nbsp;</div>
+  <div class="col-3"><h5> OUR VISION<hr / style="color:#000000"></h5><h6>To become centre of excellence in Pharmacy education, research, and training and to provide human resource with attitude skill knowledge and ethics.</h6></div>
+  <div class="col-3"><h5>OUR MISSION </h5><hr /><h6>M1 : To impart quality Pharmacy education of high standards to students.</h6><br />
+
+<h6>M2 : To bolster an environment that promotes research attitude in students.</h6><br />
+
+<h6>M3 : To nurture the students with required skills and attitude having quest for innovation as well as ethical and professional standard to make them responsible citizens of India</h6><br />
+</div>
+<div class="col-3">
+<h5> PROGRAM EDUCATIONAL OBJECTIVES</h5><hr / style="color:#000000"><h6>PEO I: To develop ability among students to understand concept of Pharmacy with adequate hands on training and to give exposure to new technologies and emerging trends in Pharmacy.<br /><br />
+
+PEO II: To provide an environment that makes students to identify, plan and design the problem, analyze it and provide solution.<br /><br />
+
+PEO III: To inculcate in students the professionalism, optimism, effective communication skills, leadership qualities and qualities of Life - long learner.<br /><br />
+
+PEO IV: To provide an environment to make the students ethically and socially obliged, competent to undertake responsibilities of healthcare system.<br /><br />
+</h6></div></div></div>
+</div>
+<!--close-->
+<div class="container-fluid">
+  <div class="row" style="background-color:#00CC99;height:180px">
+   <div class="col-2">
+      &nbsp;
+    </div>
+    <div class="col-2" style="color:#FFFFFF; margin-top:30px" align="center">  
+      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-emoji-smile" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>
+</svg> <h1 style="padding-top:10px">430</h1><h6>STUDENTS</h6>
+    </div>
+    <div class="col-2" style="color:#FFFFFF; margin-top:30px" align="center"> <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-collection" viewBox="0 0 16 16">
+  <path d="M2.5 3.5a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-11zm2-2a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6v7zm1.5.5A.5.5 0 0 1 1 13V6a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-13z"/>
+</svg> <h1 style="padding-top:10px">03</h1><h6>OUR COURSES</h6>
+    </div>
+    <div class="col-2" style="color:#FFFFFF; margin-top:30px" align="center">
+      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+  <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+  <path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"/>
+  <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/> <h1 style="padding-top:10px">48</h1> <h6> OUR STAFF</h6>
+</svg>
+    </div>
+	<div class="col-2" align="center" style="padding-top:30px; color:#FFFFFF">
+      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-trophy-fill" viewBox="0 0 16 16">
+  <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z"/>
+</svg> <h1 style="padding-top:10px">75</h1><h6>AWARDS OWN</h6>
+    </div>
+	<div class="col-2">
+      &nbsp;
+    </div>
+  </div>
+</div>
+<br />
+<!--close-->
+
+<div class="container-fluid">
+<div class="row" >
+<div class="col-2">&nbsp;</div>
+<div class="col-6"><h1>OUR DESK <hr color="#00CCCC" /></h1></div>
+  <div class="col-4"><h1>LATEST DOWNLOADS</h1> <hr color="#00CC99" /></div></div></div>
+  <!--close-->
+  <div class="container-fluid">
+  <div class="row">
+  <div class="col-2"> &nbsp;</div>
+  <div class="col-2"><div class="card" style="width: 15rem;">
+  <img src="image/chairman.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Prof. Madan S. Jagtap</h5>
+    <p class="card-text"> Chairman</p>
+    <a style="background-color:#00CC99" href="chairmanmessage.html" class="btn btn-primary">Read More</a>
+  </div>
+</div></div>
+
+<div class="col-2"><div class="card" style="width: 15rem;">
+  <img src="image/allokar.n.hjpg.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Dr.Aloorkar N.H</h5>
+    <p class="card-text">Principal</p>
+    <a style="background-color:#00CC99" href="principalmessage.html" class="btn btn-primary"> Read More</a>
+  </div>
+</div></div>
+<div class="col-2"><div class="card" style="width: 15rem;">
+  <img src="image/registrar-kale-h-j.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Mr.Kale H.J.</h5>
+    <p class="card-text">Registrar</p>
+    <a style="background-color:#00CC99" href="#" class="btn btn-primary"> Read More</a>
+  </div>
+</div></div>
+
+
+<div class="col-2" >
+<div class="card" style="width: 18rem;">
+ <ul class="list-group list-group-flush">
+ <a href=""  <li  class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-pdf" viewBox="0 0 16 16">
+  <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
+  <path d="M4.603 14.087a.81.81 0 0 1-.438-.42c-.195-.388-.13-.776.08-1.102.198-.307.526-.568.897-.787a7.68 7.68 0 0 1 1.482-.645 19.697 19.697 0 0 0 1.062-2.227 7.269 7.269 0 0 1-.43-1.295c-.086-.4-.119-.796-.046-1.136.075-.354.274-.672.65-.823.192-.077.4-.12.602-.077a.7.7 0 0 1 .477.365c.088.164.12.356.127.538.007.188-.012.396-.047.614-.084.51-.27 1.134-.52 1.794a10.954 10.954 0 0 0 .98 1.686 5.753 5.753 0 0 1 1.334.05c.364.066.734.195.96.465.12.144.193.32.2.518.007.192-.047.382-.138.563a1.04 1.04 0 0 1-.354.416.856.856 0 0 1-.51.138c-.331-.014-.654-.196-.933-.417a5.712 5.712 0 0 1-.911-.95 11.651 11.651 0 0 0-1.997.406 11.307 11.307 0 0 1-1.02 1.51c-.292.35-.609.656-.927.787a.793.793 0 0 1-.58.029zm1.379-1.901c-.166.076-.32.156-.459.238-.328.194-.541.383-.647.547-.094.145-.096.25-.04.361.01.022.02.036.026.044a.266.266 0 0 0 .035-.012c.137-.056.355-.235.635-.572a8.18 8.18 0 0 0 .45-.606zm1.64-1.33a12.71 12.71 0 0 1 1.01-.193 11.744 11.744 0 0 1-.51-.858 20.801 20.801 0 0 1-.5 1.05zm2.446.45c.15.163.296.3.435.41.24.19.407.253.498.256a.107.107 0 0 0 .07-.015.307.307 0 0 0 .094-.125.436.436 0 0 0 .059-.2.095.095 0 0 0-.026-.063c-.052-.062-.2-.152-.518-.209a3.876 3.876 0 0 0-.612-.053zM8.078 7.8a6.7 6.7 0 0 0 .2-.828c.031-.188.043-.343.038-.465a.613.613 0 0 0-.032-.198.517.517 0 0 0-.145.04c-.087.035-.158.106-.196.283-.04.192-.03.469.046.822.024.111.054.227.09.346z"/>
+</svg>Affiliation Letter</li></a>
+   <a href="#" <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-pdf" viewBox="0 0 16 16">
+  <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
+  <path d="M4.603 14.087a.81.81 0 0 1-.438-.42c-.195-.388-.13-.776.08-1.102.198-.307.526-.568.897-.787a7.68 7.68 0 0 1 1.482-.645 19.697 19.697 0 0 0 1.062-2.227 7.269 7.269 0 0 1-.43-1.295c-.086-.4-.119-.796-.046-1.136.075-.354.274-.672.65-.823.192-.077.4-.12.602-.077a.7.7 0 0 1 .477.365c.088.164.12.356.127.538.007.188-.012.396-.047.614-.084.51-.27 1.134-.52 1.794a10.954 10.954 0 0 0 .98 1.686 5.753 5.753 0 0 1 1.334.05c.364.066.734.195.96.465.12.144.193.32.2.518.007.192-.047.382-.138.563a1.04 1.04 0 0 1-.354.416.856.856 0 0 1-.51.138c-.331-.014-.654-.196-.933-.417a5.712 5.712 0 0 1-.911-.95 11.651 11.651 0 0 0-1.997.406 11.307 11.307 0 0 1-1.02 1.51c-.292.35-.609.656-.927.787a.793.793 0 0 1-.58.029zm1.379-1.901c-.166.076-.32.156-.459.238-.328.194-.541.383-.647.547-.094.145-.096.25-.04.361.01.022.02.036.026.044a.266.266 0 0 0 .035-.012c.137-.056.355-.235.635-.572a8.18 8.18 0 0 0 .45-.606zm1.64-1.33a12.71 12.71 0 0 1 1.01-.193 11.744 11.744 0 0 1-.51-.858 20.801 20.801 0 0 1-.5 1.05zm2.446.45c.15.163.296.3.435.41.24.19.407.253.498.256a.107.107 0 0 0 .07-.015.307.307 0 0 0 .094-.125.436.436 0 0 0 .059-.2.095.095 0 0 0-.026-.063c-.052-.062-.2-.152-.518-.209a3.876 3.876 0 0 0-.612-.053zM8.078 7.8a6.7 6.7 0 0 0 .2-.828c.031-.188.043-.343.038-.465a.613.613 0 0 0-.032-.198.517.517 0 0 0-.145.04c-.087.035-.158.106-.196.283-.04.192-.03.469.046.822.024.111.054.227.09.346z"/>
+</svg> RTI Information</li></a>
+    <li class="list-group-item">&nbsp;</li>
+  </ul>
+  <div class="card-body">
+
+  </div>
+</div>
+</div>
+</div>
+</div><br />
+
+<!--close card-->
+<div class="fluid-container">
+<div class="row" style="height:650px; background-color:#CCCCCC; padding-top:120px">
+<div class="col-1">&nbsp;</div>
+<div class="col-3"><h2 style="color:#FFFFFF;">LATEST EVENT:</h2> <div class="card" style="width: 22rem;">
+  <img src="image/WhatsApp Image 2021-09-02 at 7.39.12 PM.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Shubhaschandra  bhos jyanti</h5>
+    <p class="card-text"></p>
+    <a href="jayanti.html" class="btn btn-primary">Read More</a>
+  </div>
+</div></div>
+<div class="col-3"><br /><br />
+<div class="card" style="width:22rem;">
+  <img src="image/WhatsApp Image 2021-09-02 at 7.39.12 PM(1).jpeg" class="card-img-top" alt="..." height="240px">
+  <div class="card-body">
+    <h5 class="card-title"> First year B.pharmacy parent Meet 2020-21</h5>
+    <p class="card-text"></p>
+    <a href="meetparent.html" class="btn btn-primary">Read More</a>
+  </div>
+</div></div>
+<div class="col-3"><br /><br />
+<div class="card" style="width:22rem;">
+  <img src="image/WhatsApp Image 2021-09-02 at 7.39.11 PM.jpeg" class="card-img-top" alt="..." height="260px">
+  <div class="card-body">
+    <h5 class="card-title">GPAT Qualifiers</h5>
+    <p class="card-text"></p>
+    <a href="sataragpat.html" class="btn btn-primary">Read More</a>
+  </div>
+</div></div>
+<div class="col-1">&nbsp;</div></div></div>
+
+<!--close-->
+<div class="container-fluid">
+<div class="row" style="height:300px"><h1 align="center">OUR RECRUITS</h1>
+<div class="col-2"><img src="image/WhatsApp Image 2021-09-02 at 8.26.30 PM(1).jpeg" height="150px" width="150" /></div>
+<div class="col-2"><img src="image/WhatsApp Image 2021-09-02 at 8.26.30 PM(2).jpeg" height="150px" width="150" /></div>
+<div class="col-2"><img src="image/WhatsApp Image 2021-09-02 at 8.26.30 PM.jpeg" height="150px" width="150" /></div>
+<div class="col-2"><img src="image/WhatsApp Image 2021-09-02 at 8.26.31 PM(1).jpeg" height="150px" width="150" /></div>
+<div class="col-2"><img src="image/WhatsApp Image 2021-09-02 at 8.26.31 PM.jpeg" height="150px" width="150" /></div>
+<div class="col-2"><img src="image/WhatsApp Image 2021-09-02 at 8.26.32 PM.jpeg" height="150px" width="150" /></div>
+</div></div>
+<!--close logo-->
+	<?php
+		require_once"footer.php";
+	?>
+
+
+  </body>
+</html>
